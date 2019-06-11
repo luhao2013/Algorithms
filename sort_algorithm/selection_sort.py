46c4474d0f9934cf77c1c4de77459230e51aa6fc
LuHao <luhaozxc@163.com>
2019-06-11 15:04:36 +0800

    selection_sort

---
+++ sort_algorithm/selection_sort.py
@@ -0,0 +1,17 @@
+"""
+简单选择排序：每一趟找出最小的元素和前面进行交换
+"""
+
+def selection_sort(array):
+    for i in range(0, len(array)-1):
+        small = i
+        for j in range(i+1, len(array)):
+            if array[j] < array[small]:
+                small = j
+        array[i], array[small] = array[small], array[i]
+
+
+if __name__ == "__main__":
+    testList = [1, 3, 5, 7, 2, 6, 25, 18, 13]
+
+    print(testList)
