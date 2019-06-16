5ef9e31b2c48203c597e66bc2604c6886d4989d2
LuHao <luhaozxc@163.com>
2019-06-16 21:41:42 +0800

    lettcode703数据流中第K大元素

---
+++ leetcode/703.数据流中的第K大元素.py
@@ -0,0 +1,48 @@
+"""
+设计一个找到数据流中第K大元素的类（class）。注意是排序后的第K大元素，不是第K个不同的元素。
+
+你的 KthLargest 类需要一个同时接收整数 k 和整数数组nums 的构造器，它包含数据流中的初始元素。每次调用 KthLargest.add，返回当前数据流中第K大的元素。
+
+示例:
+
+int k = 3;
+int[] arr = [4,5,8,2];
+KthLargest kthLargest = new KthLargest(3, arr);
+kthLargest.add(3);   // returns 4
+kthLargest.add(5);   // returns 5
+kthLargest.add(10);  // returns 5
+kthLargest.add(9);   // returns 8
+kthLargest.add(4);   // returns 8
+说明:
+你可以假设 nums 的长度≥ k-1 且k ≥ 1。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/kth-largest-element-in-a-stream
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+import heapq  # Python中标准库 heapq 就是小顶堆
+# 1.保存k个最大值 ---> 排序 时间复杂度 N*klog(k)
+# 2.优先队列，Min Heap size=k， 第k大的元素就永远在最上面
+# 时间复杂度O(N*logk)  堆调整的复杂度为0(logk)
+
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.pool = nums
+        self.k = k
+        heapq.heapify(self.pool)
+        while len(self.pool) > k:
+            heapq.heappop(self.pool)
+
+    def add(self, val: int) -> int:
+        if len(self.pool) < self.k:
+            heapq.heappush(self.pool, val)
+        elif val > self.pool[0]:
+            heapq.heapreplace(self.pool, val)
+        return self.pool[0]
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
