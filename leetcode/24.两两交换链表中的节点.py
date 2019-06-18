a2baff77398090e68195efa86c5296451489b0c8
LuHao <luhaozxc@163.com>
2019-06-18 10:32:58 +0800

    leetcode24两两交换链表中的节点

---
+++ leetcode/24.两两交换链表中的节点.py
@@ -0,0 +1,35 @@
+"""
+给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+ 
+
+示例:
+
+给定 1->2->3->4, 你应该返回 2->1->4->3.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev, prev.next = self, head  # self是对象，prev=self是对象间的赋值
+        while prev.next and prev.next.next:
+            a = prev.next
+            b = a.next
+            prev.next, b.next, a.next = b, a, b.next
+            prev = a
+        return self.next
