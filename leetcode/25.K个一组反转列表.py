b69e8c12e5bfe163e0844679230972642e80cd22
LuHao <luhaozxc@163.com>
2019-06-19 09:34:15 +0800

    'leetcodeK个一组反转列表'

---
+++ leetcode/25.K个一组反转列表.py
@@ -0,0 +1,81 @@
+"""
+给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+
+k 是一个正整数，它的值小于或等于链表的长度。
+
+如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+
+示例 :
+
+给定这个链表：1->2->3->4->5
+
+当 k = 2 时，应当返回: 2->1->4->3->5
+
+当 k = 3 时，应当返回: 3->2->1->4->5
+
+说明 :
+
+你的算法只能使用常数的额外空间。
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/reverse-nodes-in-k-group
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        # 1. 尾插法1
+        dummy = ListNode(0)
+        dummy.next = head
+        pre = dummy
+        tail = dummy
+        while True:
+            count = k
+            while count and tail:
+                count -= 1
+                tail = tail.next
+            if not tail: break
+            head = pre.next
+            while pre.next != tail:
+                cur = pre.next # 获取下一个元素
+                # pre与cur.next连接起来,此时cur(孤单)掉了出来
+                pre.next = cur.next
+                cur.next = tail.next # 和剩余的链表连接起来
+                tail.next = cur #插在tail后面
+            # 改变 pre tail 的值
+            pre = head
+            tail = head
+        return dummy.next
+
+        # 2. 尾插法2
+        pre, pre.next = self, head
+        tail = pre
+        while True:
+            count = k
+            while tail and count:
+                tail = tail.next
+                count -= 1
+            if not tail:
+                break
+            cur = pre.next
+            temp = cur
+            while cur is not tail:
+                #  我们尾插法的意思就是,依次把cur移到tail后面
+                cur.next, tail.next, pre.next = tail.next, cur, cur.next
+                cur = pre.next
+            pre = temp
+            tail = pre
+        return self.next
