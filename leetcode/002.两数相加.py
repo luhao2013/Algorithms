b8ab96de5fc651b00c592557e93692ab729fb539
LuHao <luhaozxc@163.com>
2019-06-22 11:15:51 +0800

    'leetcode2两数相加'

---
+++ leetcode/002.两数相加.py
@@ -0,0 +1,51 @@
+"""
+给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例：
+
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/add-two-numbers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummyhead = ListNode(0)
+        cur = dummyhead
+        carry = 0
+        # 视为将短的序列自动补0
+        while l1 or l2:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            sum = x + y + carry
+            cur.next = ListNode(sum % 10)
+            cur = cur.next
+            carry = sum // 10
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        if carry:
+            cur.next = ListNode(carry)
+        return dummyhead.next
