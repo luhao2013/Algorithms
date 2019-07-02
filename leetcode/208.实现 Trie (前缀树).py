480791a7b2a5324e122b39b133d4c285683b3e71
LuHao <luhaozxc@163.com>
2019-07-02 14:55:44 +0800

    'leetcode208实现前缀树'

---
+++ leetcode/208.实现 Trie (前缀树).py
@@ -0,0 +1,50 @@
+"""
+实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
+
+示例:
+
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // 返回 true
+trie.search("app");     // 返回 false
+trie.startsWith("app"); // 返回 true
+trie.insert("app");
+trie.search("app");     // 返回 true
+说明:
+
+你可以假设所有的输入都是由小写字母 a-z 构成的。
+保证所有输入均为非空字符串。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/implement-trie-prefix-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+class Trie(object):
+    def __init__(self):
+        self.root = {}
+        self.end_of_word = "#"
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            node = node.setdefault(char, {})  # 如果字典中包含有给定键，则返回该键对应的值，否则返回为该键设置的值。
+        node[self.end_of_word] = self.end_of_word
+
+    def search(self, word):
+        node = self.root
+        for char in word:
+            if char not in node:
+                return False
+            node = node[char]
+        return self.end_of_word in node
+
+    def startsWith(self, prefix):
+        node = self.root
+        for char in prefix:
+            if char not in node:
+                return False
+            node = node[char]
+        return True
