15be7b60381859cca5841ff311b826b70c4938a0
LuHao <luhaozxc@163.com>
2019-07-30 09:50:43 +0800

    'leetcode105从前序与中序遍历序列构造二叉树'

---
+++ leetcode/105.从前序与中序遍历序列构造二叉树.py
@@ -0,0 +1,48 @@
+"""
+根据一棵树的前序遍历与中序遍历构造二叉树。
+
+注意:
+你可以假设树中没有重复的元素。
+
+例如，给出
+
+前序遍历 preorder = [3,9,20,15,7]
+中序遍历 inorder = [9,3,15,20,7]
+返回如下的二叉树：
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if len(preorder) == 0:
+            return None
+        rootvalue = preorder[0]
+        root = TreeNode(rootvalue)
+        inorder_index = inorder.index(rootvalue)
+
+        root.left = self.buildTree(preorder[1:inorder_index + 1], inorder[0:inorder_index])
+        root.right = self.buildTree(preorder[inorder_index + 1:], inorder[inorder_index + 1:])
+
+        return root
