83aa26e6d3a2eaab34a9228389ee15f5e251f084
LuHao <luhaozxc@163.com>
2019-08-20 13:26:38 +0800

    '剑指offer28字符串的全排列'

---
+++ offer/28.字符串的全排列.py
@@ -0,0 +1,21 @@
+"""
+输入一个字符串，输出该字符串中字符的全排列。
+"""
+
+
+def my_permutation(s):
+    str_set = []
+    ret = []  # 返回结果
+
+    def permutation(string):
+        for i in string:
+            str_tem = string.replace(i, '')
+            str_set.append(i)
+            if len(str_tem) > 0:
+                permutation(str_tem)
+            else:
+                ret.append(''.join(str_set))
+            str_set.pop()
+    permutation(s)
+    return ret
+print(my_permutation('abc'))
