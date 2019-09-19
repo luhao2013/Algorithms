ca933d44e459d81340786092db003ebf04af7de0
LuHao <luhaozxc@163.com>
2019-09-19 16:54:51 +0800

    'leetcode006'

---
+++ leetcode/006.Z字形变换.py
@@ -0,0 +1,58 @@
+"""
+将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+
+比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+
+L   C   I   R
+E T O E S I I G
+E   D   H   N
+之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+
+请你实现这个将字符串进行指定行数变换的函数：
+
+string convert(string s, int numRows);
+示例 1:
+
+输入: s = "LEETCODEISHIRING", numRows = 3
+输出: "LCIRETOESIIGEDHN"
+示例 2:
+
+输入: s = "LEETCODEISHIRING", numRows = 4
+输出: "LDREOEIIECIHNTSG"
+解释:
+
+L     D     R
+E   O E   I I
+E C   I H   N
+T     S     G
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/zigzag-conversion
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1:
+            return s
+        res = ''
+        length = len(s)
+        step = 2 * numRows - 2  # 步长为2n-2
+        for row in range(numRows):
+            index = row
+            add = 2 * row
+            while index < length:
+                res += s[index]
+                if row == 0 or row == numRows - 1:  # 首行和尾行是正常步长
+                    index += step
+                else:  # 不是首行，步长是2n-2-2*row 和 2*row的交替
+                    add = step - add
+                    index += add
+
+        return res
